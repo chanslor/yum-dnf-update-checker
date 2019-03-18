3da3df812dad4046d9cbbb9405e92d3d70150a7f
Mike Chanslor <mdchansl@southernco.com>
2019-03-18 14:26:44 -0500

    Check now works as function

--- check_updates.bash
+++ check_updates.bash
@@ -21,6 +21,7 @@ RUNLEVEL=$(runlevel | cut -d" " -f2)
 
 if [[ "$RUNLEVEL" -eq "3" || "$RUNLEVEL" -eq "5" ]]; then
 echo "You are at runlevel 3 or 5"
+echo "Check for Security updates..."
 return 0
 else
 echo "I don't know your runlevel"
@@ -31,34 +32,29 @@ fi
 
 check_updates() {
 
-#Remove specific user stuff to config file
-#/etc/sysconfig/check-update.conf
-#USER=mdchansl
-
-if [[ "$(sudo yum updateinfo 2>&1 | /bin/grep "Security" | wc -l)" -ge "1" ]] ; then
-notify-send -u critical -t 999999999 "UPDATES" "You have pending UPDATES" --icon=dialog-information
-echo "You have pending UPDATES" | write mdchansl pts/1
-sudo echo "===============================================================================" > /var/log/ems_yum_updates/${DATE}.log 2>&1
-sudo date >> /var/log/ems_yum_updates/${DATE}.log 2>&1
-sudo yum updateinfo info >> /var/log/ems_yum_updates/${DATE}.log 2>&1
+if [[ "$(yum updateinfo 2>&1 | /bin/grep "Security" | wc -l)" -ge "1" ]] ; then
+echo "===============================================================================" > /var/log/ems_yum_updates/${DATE}.log 2>&1
+date >> /var/log/ems_yum_updates/${DATE}.log 2>&1
+yum updateinfo info >> /var/log/ems_yum_updates/${DATE}.log 2>&1
+return 100
 fi
 
 }
 
 notify_users() {
-	USERS=$(/usr/bin/w | cut -d" " -f1 | sed -e '/^$/d' | /bin/grep -v USER | sort | uniq)
-	for i in $USERS
-	do
-echo "You have pending OS UPDATES.
+echo -e "
+
+\U0001F534 You have pending OS Security Updates.
 
  --- $TODAY ---
 This system, $SYSTEM is scheduled for reboot at $OUTAGE_TIME 
-See /var/log/ems_yum_updates/ for details on patches being applied." | /bin/wall
+See /var/log/ems_yum_updates/ for details on patches being applied."
+#See /var/log/ems_yum_updates/ for details on patches being applied." | /bin/wall
 
-	done
 }
 
 runlevel_is || echo "Not at correct runlevel."
 
+check_updates || notify_users
 
 
