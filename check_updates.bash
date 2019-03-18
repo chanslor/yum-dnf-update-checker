4ae3e5dc139eeb229ab223bae993baeb90367a08
Mike Chanslor <mdchansl@southernco.com>
2019-03-18 10:50:51 -0500

    Turning into functions

--- check_updates.bash
+++ check_updates.bash
@@ -4,6 +4,7 @@
 #mdchansl@southernco.com
 
 #Check for pending security patches
+#Add dir stuff to rpm install
 if [ ! -d /var/log/ems_yum_updates ] ; then
 sudo mkdir -p /var/log/ems_yum_updates
 sudo chmod 775 /var/log/ems_yum_updates
@@ -11,8 +12,12 @@ sudo chown root:unixadmin /var/log/ems_yum_updates
 fi
 
 DATE=$(date +%F)
+SYSTEM=$(uname -n)
+TODAY=$(date)
+OUTAGE_TIME="NULL"
 
-#if [ "$(sudo yum updateinfo 2>&1 | /bin/egrep "Bugfix|Enhancement|Security")" > "1" ] ; then
+check_updates() {
+
 if [[ "$(sudo yum updateinfo 2>&1 | /bin/grep "Security" | wc -l)" -ge "1" ]] ; then
 notify-send -u critical -t 999999999 "UPDATES" "You have pending UPDATES" --icon=dialog-information
 echo "You have pending UPDATES" | write mdchansl pts/1
@@ -20,3 +25,27 @@ sudo echo "=====================================================================
 sudo date >> /var/log/ems_yum_updates/${DATE}.log 2>&1
 sudo yum updateinfo info >> /var/log/ems_yum_updates/${DATE}.log 2>&1
 fi
+
+}
+
+notify_users() {
+	USERS=$(/usr/bin/w | cut -d" " -f1 | sed -e '/^$/d' | /bin/grep -v USER | sort | uniq)
+	for i in $USERS
+	do
+echo "You have pending OS UPDATES.
+
+ --- $TODAY ---
+This system, $SYSTEM is scheduled for reboot at $OUTAGE_TIME 
+See /var/log/ems_yum_updates/ for details on patches being applied." | /bin/wall
+
+	done
+}
+
+#Look at the Amazon code: update-motd.py
+
+#if [ "$(sudo yum updateinfo 2>&1 | /bin/egrep "Bugfix|Enhancement|Security")" > "1" ] ; then
+
+#Remove specific user stuff to config file
+#/etc/sysconfig/check-update.conf
+#USER=mdchansl
+
